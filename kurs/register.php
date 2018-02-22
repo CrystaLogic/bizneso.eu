a2c845ff9469a0a35e1e6f017d9a5a6d09acf1df
Unknown <morwa666@gmail.com>
2018-02-22 23:37:51 +0100

    Register form

---
+++ kurs/register.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kurs</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat:600,700&amp;subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <header id="main">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-2">
+            <img class="w-100" src="img/logo.png" alt="bizneso.eu">
+          </div>
+          <div class="col-lg-9">
+            <ul class="nav justify-content-end">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Tydzień 1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Tydzień 2</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Tydzień 3</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Tydzień 4</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-1 icons d-flex align-items-center justify-content-between">
+            <a href="#">
+              <i class="material-icons">settings</i>
+            </a>
+            <a href="#">
+              <i class="material-icons">exit_to_app</i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+    <section id="login">
+      <div class="container">
+        <!-- Spacer -->
+        <div class="w-100 py-4"></div>
+        <!-- Spacer end -->
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <form class="needs-validation" action="add-user.php" method="POST" novalidate>
+              <fieldset class="form-group">
+                <label for="email">Email</label>
+                <input type="email" required class="form-control" name="email" id="email" placeholder="Enter email">
+              </fieldset>
+              <fieldset class="form-group">
+                <label for="password">Hasło</label>
+                <input type="password" required class="form-control" name="password" id="password" placeholder="Hasło">
+              </fieldset>
+              <fieldset class="form-group">
+                <label for="name">Imię i Nazwisko/Nazwa Firmy</label>
+                <input type="text" class="form-control" name="name" id="name" placeholder="Imię i Nazwisko/Nazwa Firmy">
+                <small class="form-text text-muted">Opcjonalne</small>
+              </fieldset>
+              <fieldset class="form-group">
+                <label for="address">Adres</label>
+                <input type="text" class="form-control" name="address" id="address" placeholder="Adres">
+                <small class="form-text text-muted">Opcjonalne</small>
+              </fieldset>
+              <fieldset class="form-group">
+                <label for="nip">NIP (pozostaw puste jeżeli zakładasz konto dla osoby fizycznej)</label>
+                <input type="text" class="form-control" name="nip" id="nip" placeholder="NIP">
+                <small class="form-text text-muted">Opcjonalne</small>
+              </fieldset>
+              <fieldset class="form-group">
+                <label for="phone">Telefon</label>
+                <input type="tel" class="form-control" name="phone" id="phone" placeholder="Telefon">
+                <small class="form-text text-muted">Opcjonalne</small>
+              </fieldset>
+              <button type="submit" class="btn btn-primary">Zarejestruj się</button>
+            </form>
+            <script>
+
+              (function()
+              {
+                'use strict';
+                window.addEventListener('load', function()
+                {
+                  var forms = document.getElementsByClassName('needs-validation');
+                  var validation = Array.prototype.filter.call(forms, function(form)
+                  {
+                    form.addEventListener('submit', function(event)
+                    {
+                      if(form.checkValidity() === false)
+                      {
+                        event.preventDefault();
+                        event.stopPropagation();
+                      }
+
+                      form.classList.add('was-validated');
+                    }, false);
+                  });
+                }, false);
+              })();
+
+            </script>
+          </div>
+        </div>
+
+      </div>
+
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
